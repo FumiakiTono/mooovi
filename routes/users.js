03ed5a8de78a7da99832c2f93c0b4d02118fc5b5
FumiakiTono <tonotorious@gmail.com>
2018-11-25 19:30:14 +0900

    create-user-function

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var knex = require('knex')({
+  dialect: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mooovi_express',
+    charset: 'utf8'
+  }
+});
+
+var Bookshelf = require('bookshelf')(knex);
+
+var User = Bookshelf.Model.extend({
+  tableName: 'users'
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,17 +27,115 @@ router.get('/', function(req, res, next) {
 
 router.get('/signup', (req, res, next) => {
   var data = {
-    title: "新規登録"
+    title: "新規登録",
+    form: {email: '', password: '', nickname: ''},
+    content: ""
   }
   res.render('users/signup', data);
 });
 
+router.post('/add', (req, res, next) => {
+  var request = req;
+  var response = res;
+  // バリデーション
+  req.check('email', "emailは必ず入力して下さい。").notEmpty();
+  req.check('password', "PASSWORDは必ず入力して下さい。").notEmpty();
+  req.getValidationResult().then((result) => {
+    if(!result.isEmpty()){
+      var content = '<ul class="error">';
+      var result_arr = result.array();
+      for(var n in result_arr){
+        content += '<li>' + result_arr[n].msg + '</li>'
+      }
+      content += '</li>';
+      var data = {
+        title: '新規登録',
+        content: content,
+        form: req.body
+      }
+      response.render('users/signup', data);
+    } else {
+      request.session.login = null;
+      new User(req.body).save().then((model) => {
+        response.redirect('/');
+      });
+    }
+  })
+
+});
+
+
 router.get('/signin', (req, res, next) => {
   var data = {
-    title: "ログイン"
+    title: "ログイン",
+    content: ""
   }
   res.render('users/signin', data);
 });
+
+router.post('/signin', (req, res, next) => {
+  var request = req;
+  var response = res;
+  // バリデーション
+  req.check('email', "emailは必ず入力して下さい。").notEmpty();
+  req.check('password', "PASSWORDは必ず入力して下さい。").notEmpty();
+  req.getValidationResult().then((result) => {
+    if (!result.isEmpty()) {
+      var content = '<ul class="error">';
+      var result_arr = result.array();
+      for(var n in result_arr){
+        content += '<li>' + result_arr[n].msg + '</li>'
+      }
+      content += '</ul>';
+      var data = {
+        title: 'Login',
+        content: content,
+        form: req.body
+      }
+      response.render('users/signin', data);
+    } else {
+      var em = req.body.email;
+      var pw = req.body.password;
+
+      User.query({where: {email: em}, andWhere: {password: pw}})
+            .fetch()
+            .then((model) => {
+              if (model == null){
+                  var data = {
+                    title: '再入力',
+                    content:'<p class="error">名前またはパスワードが違います。</p>',
+                    form: req.body
+                  };
+                  response.render('users/signin', data);
+              } else {
+                request.session.login = model.attributes;
+                var data = {
+                  title: 'Login',
+                  content: '<p>ログインしました！</p>',
+                  form: req.body
+                };
+                response.render('index', data);
+              }
+      });
+    }
+  })
+});
+
+// ログアウト、sessionの使い方合っているか？
+router.get('/signout', (req, res, next) => {
+  var request = req;
+  var response = res;
+  // console.log(req.session);
+  req.session.login = null;
+  // console.log(req.session);
+  var data = {
+    title: 'Signout',
+    content: '<p>ログアウトしました！</p>',
+    form: req.body
+  };
+  response.render('users/signin', data);
+});
+
 
 router.get('/:id', (req, res, next) => {
   var data = {
@@ -26,5 +143,6 @@ router.get('/:id', (req, res, next) => {
   }
   res.render('users/show', data);
 });
+
 
 module.exports = router;
