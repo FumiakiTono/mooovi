8edc6fe8dcbdca631041012bc576b48237770718
FumiakiTono <tonotorious@gmail.com>
2018-12-02 20:23:24 +0900

    create-add-review-function-and-add-search-function

--- routes/index.js
+++ routes/index.js
@@ -21,25 +21,28 @@ var User = Bookshelf.Model.extend({
 });
 
 var Product = Bookshelf.Model.extend({
-  tableName: 'products'
+  tableName: 'products',
+  hasTimestamps: true
 });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  console.log("1");
   new Product().fetchAll().then((collection) => {
+    // console.log(collection);
     var data = {
       title: "トップページ",
       content: "",
-      // login: req.session.login,
+      login: req.session.login,
       collection: collection.toArray()
     };
-    // console.log(data.collection.attributes.title);
     res.render("index", data);
   }).catch((err) => {
     res.render(500).json({error: true, data: {message: err.message}});
   });
 
+  // コメントアウトを外すと上のres.renderとダブる
   // if (req.session.login==null){
   //   var data = {
   //     title: "トップページ",
